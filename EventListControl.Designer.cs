c7b7e15e26d3b67abf7608c1e9eaa01b4ea641e3
Equinox_08 <stewhut221@gmail.com>
2026-05-27 09:07:14 +0800

    Modified Events to Connect for Event view via API

--- EventListControl.Designer.cs
+++ EventListControl.Designer.cs
@@ -28,36 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dgvEvents = new System.Windows.Forms.DataGridView();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEvents)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dgvEvents
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1241, 745);
-            this.dataGridView1.TabIndex = 0;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dgvEvents.AllowUserToAddRows = false;
+            this.dgvEvents.AllowUserToDeleteRows = false;
+            this.dgvEvents.AllowUserToResizeColumns = false;
+            this.dgvEvents.AllowUserToResizeRows = false;
+            this.dgvEvents.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvEvents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEvents.Location = new System.Drawing.Point(39, 47);
+            this.dgvEvents.Name = "dgvEvents";
+            this.dgvEvents.RowHeadersWidth = 51;
+            this.dgvEvents.RowTemplate.Height = 24;
+            this.dgvEvents.Size = new System.Drawing.Size(1169, 605);
+            this.dgvEvents.TabIndex = 0;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(996, 672);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(135, 46);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "REFRESH";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
             // 
             // EventListControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.dgvEvents);
             this.Name = "EventListControl";
             this.Size = new System.Drawing.Size(1241, 745);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEvents)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvEvents;
+        private System.Windows.Forms.Button btnRefresh;
     }
 }
--- EventListControl.cs
+++ EventListControl.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Data;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Windows.Forms;
-using Newtonsoft.Json.Linq;
 
 namespace AP_Project
 {
@@ -11,11 +12,70 @@ namespace AP_Project
         public EventListControl()
         {
             InitializeComponent();
+            this.Load += async (s, e) => await LoadEvents();
+
+        }
+        private async Task LoadEvents()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string url = "http://localhost:8080/api/events/list.php";
+
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    string json = await response.Content.ReadAsStringAsync();
+
+                    JObject obj = JObject.Parse(json);
+
+                    if (obj["status"]?.ToString() != "success")
+                    {
+                        MessageBox.Show("Failed to load events");
+                        return;
+                    }
+
+                    DataTable table = new DataTable();
+
+                    JArray data = (JArray)obj["data"];
+
+                    if (data.Count == 0)
+                    {
+                        dgvEvents.DataSource = table;
+                        return;
+                    }
+
+                    foreach (JProperty prop in ((JObject)data[0]).Properties())
+                    {
+                        table.Columns.Add(prop.Name);
+                    }
+
+                    foreach (JObject item in data)
+                    {
+                        DataRow row = table.NewRow();
+
+                        foreach (JProperty prop in item.Properties())
+                        {
+                            row[prop.Name] = prop.Value?.ToString();
+                        }
+
+                        table.Rows.Add(row);
+                    }
+
+                    dgvEvents.DataSource = table;
+                    dgvEvents.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                    dgvEvents.ReadOnly = true;
+                    dgvEvents.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("API Error: " + ex.Message);
+            }
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private async void btnRefresh_Click(object sender, EventArgs e)
         {
-
+            await LoadEvents();
         }
     }
 }
